fc133ac257fa5ea0583caad976bc53d93a8de32f
Nikhil Prabhakar <nikhil@wyndescorpion.local>
2017-01-08 14:34:50 -0500

    added flash support

--- raspi-setup.sh
+++ raspi-setup.sh
@@ -25,7 +25,7 @@ sudo apt-get install okteta vim -y
 wget -qO - http://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
 echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install chromium-browser rpi-youtube -y
+sudo apt-get install chromium-browser rpi-chromium-mods rpi-youtube -y
 
 # update chromium regularly
 sudo apt-get update
